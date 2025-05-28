53893403658a18a45363a00521eb00e619958ab1
asuka <asuka@asuka>
2025-05-28 11:26:48 +0800

    新增

---
+++ src/main/java/ask/azurlane/AzurlaneApplication.java
@@ -0,0 +1,13 @@
+package ask.azurlane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurlaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurlaneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ask/azurlane/AzurlaneApplicationTests.java
@@ -0,0 +1,13 @@
+package ask.azurlane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzurlaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
